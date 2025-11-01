94a829f1de09a2a9a69a63a4388eeab4bea8cf6e
Vera_Facundo <facundovera647@gmail.com>
2025-11-01 19:29:18 -0300

    cambio en el id del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,9 +7,9 @@ function Formulario() {
     <section className="container mt-4 p-4 border rounded shadow bg-dark text-white ">
       <h1 className="text-center">Bienvenidos</h1>
       <Form className="w-75 mx-auto mt-3">
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="textoTarea">
           <div className="d-flex justify-content-center align-items-center gap-2 mt-4">
-            <Form.Control type="email" placeholder="Ingrese la tarea" />
+            <Form.Control type="text" placeholder="Ingrese la tarea" />
             <Button variant="primary" type="submit">
               Submit
             </Button>
